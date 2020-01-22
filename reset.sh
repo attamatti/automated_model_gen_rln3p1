328966410de151584bfdf234d67e0ee9c35100ee
Matthew Iadanza <fbsmi@absl-wkstn01.leeds.ac.uk>
2020-01-22 16:45:13 +0000

    added cleanup script

---
+++ reset.sh
@@ -0,0 +1,15 @@
+rm default_pipeline.star
+rm .gui_*
+rm -r .Nodes
+rm -r Import
+rm -r MotionCorr
+rm -r .relion*
+rm -r Schedules
+rm -r InitialModel
+rm -r filtered
+rm -r External
+rm -r CtfFind
+rm -r Class2D
+rm -r Extract
+rm -r config.json
+rm mail.txt
